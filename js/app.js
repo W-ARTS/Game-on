f35e9004236fa8bcdb731a800331099766f6c7f1
Wael <you@example.com>
2022-12-11 16:00:35 +0100

    ok

--- js/app.js
+++ js/app.js
@@ -58,9 +58,12 @@ const setSuccess = element => {
 
 //Verifier si l'email est valide
 const isValidEmail = email => {
-    const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    return re.test(String(email).toLocaleLowerCase());
-}
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
 
 //Validation du formulaire
 
